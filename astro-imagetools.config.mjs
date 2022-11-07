36176108ed89637a8653242bde173990979ac9bc
ajzliu <42823422+ajzliu@users.noreply.github.com>
2022-11-06 23:49:42 -0800

    Add more image breakpoints

--- astro-imagetools.config.mjs
+++ astro-imagetools.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro-imagetools/config";
 
 export default defineConfig({
-  breakpoints: { count: 3, minWidth: 300, maxWidth: 1800 },
+  breakpoints: { count: 5, minWidth: 300, maxWidth: 1800 },
 });
