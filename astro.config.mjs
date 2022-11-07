1e3726ac47be13ae2284d1fb2baeb6c01f08c9d0
ajzliu <42823422+ajzliu@users.noreply.github.com>
2022-11-06 22:08:28 -0800

    Move to astro-imagetools

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,24 +7,20 @@ import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 
 // https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 import prefetch from "@astrojs/prefetch";
 
 import robotsTxt from "astro-robots-txt";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.ajzliu.com",
   integrations: [
     tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
+    astroImageTools,
     sitemap(),
     prefetch(),
     robotsTxt(),
