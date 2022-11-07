d641c427dd8bca62d787e86b5071dfc554760ca8
ajzliu <42823422+ajzliu@users.noreply.github.com>
2022-11-06 23:26:17 -0800

    Fix imagetools config

---
+++ astro-imagetools.config.mjs
@@ -0,0 +1,5 @@
+import { defineConfig } from "astro-imagetools/config";
+
+export default defineConfig({
+  breakpoints: { count: 3, minWidth: 300, maxWidth: 1800 },
+});
--- astro.config.mjs
+++ astro.config.mjs
@@ -14,12 +14,10 @@ import prefetch from "@astrojs/prefetch";
 
 import robotsTxt from "astro-robots-txt";
 import { astroImageTools } from "astro-imagetools";
-import { defineConfig } from "astro-imagetools/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.ajzliu.com",
-  breakpoints: { count: 3, minWidth: 300, maxWidth: 1800 },
   integrations: [
     tailwind(),
     astroImageTools,
