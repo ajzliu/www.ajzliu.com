ccbe79e49ab97a1cf9a033ae9e1564aab709b793
ajzliu <42823422+ajzliu@users.noreply.github.com>
2022-11-06 16:09:15 -0800

    Add Astro integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,30 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://www.ajzliu.com",
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    sitemap(),
+    prefetch(),
+    robotsTxt(),
+    compress(),
+  ],
 });
